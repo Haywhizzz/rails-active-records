b070b1a739c0fb80c85bf6c2a26dd5ecf5aee300
haywhizzz <haywhizzz@gmail.com>
2020-09-28 17:49:49 +0100

    add necessary validations

--- db/schema.rb
+++ db/schema.rb
@@ -21,11 +21,10 @@ ActiveRecord::Schema.define(version: 2020_09_28_142753) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.text "body"
     t.integer "user_id"
+    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|

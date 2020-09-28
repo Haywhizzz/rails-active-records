5fb6621a708677eb22cbeed710eb3ea25788b1ff
haywhizzz <haywhizzz@gmail.com>
2020-09-28 19:41:00 +0100

    add email attribute to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: { minimum: 5, maximum: 15 }, uniqueness: true
+  validates :name, presence: true, length: { minimum: 5, maximum: 15 }, uniqueness: true
+  validates :email, presence: true, uniqueness: true
   has_many :posts
   has_many :comments, through: :posts
 end

276cba918e5501d293cc95829f4d29003482e491
haywhizzz <haywhizzz@gmail.com>
2020-09-28 19:44:18 +0100

    remove title attr. from comments model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :post
   has_one :user, through: :post
 
-  validates :title, presence: true, length: { minimum: 5, maximum: 30 }
   validates :body, presence: true, length: { minimum: 5, maximum: 150 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { minimum: 5, maximum: 15 }, uniqueness: true
   validates :email, presence: true, uniqueness: true
   has_many :posts
-  has_many :comments, through: :posts
+  has_many :comments
 end
